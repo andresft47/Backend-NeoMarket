a7d2fda1c3a511c1fe2fc3275b43b91320d9c715
Lucho-ctrl <kipotosqui33@gmail.com>
2026-05-22 21:35:47 -0500

    Correcion extension Spring

---
+++ src/main/java/com/neomarket/backend/BackendApplication.java
@@ -0,0 +1,22 @@
+package com.neomarket.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.neomarket.backend",
+    "com.neomarket.controller",
+    "com.neomarket.service",
+    "com.neomarket.config"
+})
+@EntityScan(basePackages = "com.neomarket.model")
+@EnableJpaRepositories(basePackages = "com.neomarket.repository")
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
